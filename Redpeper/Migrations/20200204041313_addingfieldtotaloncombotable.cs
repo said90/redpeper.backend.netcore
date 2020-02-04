f23b0dfe17cbe6f02d06ca6f9797921deb188ee7
said1606 <bryan.franco37@gmail.com>
2020-02-03 22:15:11 -0600

    adding field total in combo

---
+++ Redpeper/Migrations/20200204041313_addingfieldtotaloncombotable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Redpeper.Migrations
+{
+    public partial class addingfieldtotaloncombotable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Total",
+                table: "Combos",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "Combos");
+        }
+    }
+}
--- Redpeper/Model/Combo.cs
+++ Redpeper/Model/Combo.cs
@@ -10,6 +10,7 @@ namespace Redpeper.Model
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public double Total { get; set; }
         public virtual List<ComboDetail> ComboDetails { get; set; }
     }
 }
