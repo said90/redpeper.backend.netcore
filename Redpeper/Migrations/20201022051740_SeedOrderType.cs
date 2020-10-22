7493e48f9a6b33905019619a5806c6efa70fe613
Said Franco <bryan.franco37@gmail.com>
2020-10-21 23:19:41 -0600

    Adding seed for order types

---
+++ Redpeper/Migrations/20201022051740_SeedOrderType.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Redpeper.Migrations
+{
+    public partial class SeedOrderType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "INSERT INTO public.\"OrderTypes\" (\"Id\", \"Name\") VALUES (1, 'En Restaurante');\nINSERT INTO public.\"OrderTypes\" (\"Id\", \"Name\") VALUES (2, 'Para Llevar');\nINSERT INTO public.\"OrderTypes\" (\"Id\", \"Name\") VALUES (3, 'A domicilio');");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
